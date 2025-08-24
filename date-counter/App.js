a7e02f3a52cad9106cc2744fd79c835a083f11d0
Ridho Rifqy Muhammad <ridhorifqy34@gmail.com>
2025-08-24 20:53:03 +0700

    new feature of date counter that using slider and input

--- date-counter/App.js
+++ date-counter/App.js
@@ -14,11 +14,16 @@ export default function App() {
   }
 
   function handlePlusCount() {
-    setCount((c) => c + 1);
+    setCount((c) => c + step);
   }
 
   function handleMinCount() {
-    setCount((c) => c - 1);
+    setCount((c) => c - step);
+  }
+
+  function handleReset() {
+    setStep(1);
+    setCount(0);
   }
 
   const date = new Date();
@@ -27,18 +32,35 @@ export default function App() {
   return (
     <div className="App">
       <div className="btn">
-        <button onClick={handleMinStep}>-</button>
-        <p>Step: {step}</p>
-        <button onClick={handlePlusStep}>+</button>
+        <input
+          type="range"
+          id="step"
+          name="step"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        {/* <button onClick={handleMinStep}>-</button> */}
+        <p>{step}</p>
+        {/* <button onClick={handlePlusStep}>+</button> */}
       </div>
       <div className="btn">
         <button onClick={handleMinCount}>-</button>
-        <p>Count: {count}</p>
+        <input
+          type="text"
+          name="input"
+          id="input"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+        {/* <p>Count: {count}</p> */}
         <button onClick={handlePlusCount}>+</button>
       </div>
       <p>
         {count} days from today is {date.toDateString()}
       </p>
+      <button onClick={handleReset}>Reset</button>
     </div>
   );
 }
